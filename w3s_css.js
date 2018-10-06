bbd0a85c15d2971350518f5d41cbbc2f3bc8f32e
Tim Howarth <tim.howarth13@gmail.com>
2018-10-06 11:55:57 +0000

    Commit at shutdown

--- w3s_css.js
+++ w3s_css.js
@@ -2,18 +2,17 @@ const http = require('http'); //declare module for server create.
 const fs = require('fs'); // declare module for file operations.
 const port=process.env.PORT || 3000 // declare port for heroku.
 // initialise variables for page creation.
-var headertop="<html><head>";
-var headerbot="</head><body>";
-var footer="</body></html>";
-var data=headertop;
-// next 2 instructions could be looped through list of REST parameters.
-fs.readFile('index_head.html', function(err, data1) {
-  data=data+data1+headerbot;
+//var headertop="<html><head>";
+//var headerbot="</head><body>";
+//var footer="</body></html>";
+//var data=headertop;
+var data="";
+fs.readFile('w3s_css.html', function(err, data1) {
+  data=data+data1;
 });
-//remove this comment
-fs.readFile('index_body.html', function(err, data2) {
-  data=data+data2+footer;
-});
+//fs.readFile('index_body.html', function(err, data2) {
+//  data=data+data2+footer;
+//});
 // server responds.
 var server = http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
