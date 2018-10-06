b34469bebdc2c691d696ab6433ccb44d4602eaf7
Tim Howarth <tim.howarth13@gmail.com>
2018-10-06 14:56:22 +0000

    Commit at shutdown

--- whatsnearme.js
+++ whatsnearme.js
@@ -8,11 +8,31 @@ var pageinfo="Last refresh: "+dt.myDateTime()+"</br>"+"<a href='https://github.c
 var footer="</body></html>";
 var data=header+pageinfo;
 // next 2 instructions could be looped through list of REST parameters.
-fs.readFile('demofile1.html', function(err, data1) {
-  data=data+data1; 
+fs.readFile('a1_open_form.html', function(err, data1) {
+  data=data+data1;
 });
-fs.readFile('demofile2.html', function(err, data2) {
-  data=data+data2+footer
+fs.readFile('b1_open_map_frameset.html', function(err, data1) {
+  data=data+data1;
+});
+fs.readFile('b2_close_frameset.html', function(err, data1) {
+  data=data+data1;
+});
+fs.readFile('b1_open_item_playing_frameset.html', function(err, data1) {
+  data=data+data1;
+});
+fs.readFile('b2_close_frameset.html', function(err, data1) {
+  data=data+data1;
+});
+fs.readFile('b1_open_actions_frameset.html', function(err, data1) {
+  data=data+data1;
+});
+fs.readFile('b2_close_frameset.html', function(err, data1) {
+  data=data+data1;
+});
+fs.readFile('a2_close_form.html', function(err, data1) {
+  data=data+data1;
+});
+  data=data+footer
 });
 // next instruction appears to be ignored by heroku.
 fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
@@ -23,7 +43,7 @@ fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
 var server = http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write(data);
-  res.end();  
+  res.end();
 });
 // server listensfor client request.
 server.listen(port,() => {
