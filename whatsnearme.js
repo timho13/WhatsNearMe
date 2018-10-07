f1f84bda468d4a72a45ff3b9545613181805aeaf
Tim Howarth <tim.howarth13@gmail.com>
2018-10-07 07:18:19 +0000

    Commit at shutdown

--- whatsnearme.js
+++ whatsnearme.js
@@ -11,30 +11,20 @@ var data=header+pageinfo;
 var body_code_list = {
     a: 'a1_open_form.html',
     b: 'b1_open_map_frameset.html',
-    c: 'b2_close_frameset.html'
-};
+    c: 'b2_close_frameset.html',
+    d: 'c1_open_item_playing_frameset.html',
+    e: 'b2_close_frameset.html',
+    f: 'd1_open_actions_frameset.html',
+    g: 'b2_close_frameset.html',
+    h: 'a2_close_form.html'
+});
 for (var filetoread in body_code_list){
 fs.readFile(body_code_list[filetoread], function(err, data1) {
   data=data+data1;
 });
 };
 
-fs.readFile('c1_open_item_playing_frameset.html', function(err, data1) {
-  data=data+data1;
-});
-fs.readFile('b2_close_frameset.html', function(err, data1) {
-  data=data+data1;
-});
-fs.readFile('d1_open_actions_frameset.html', function(err, data1) {
-  data=data+data1;
-});
-fs.readFile('b2_close_frameset.html', function(err, data1) {
-  data=data+data1;
-});
-fs.readFile('a2_close_form.html', function(err, data1) {
-  data=data+data1;
-});
-  data=data+footer;
+ data=data+footer;
 // server responds.
 var server = http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
