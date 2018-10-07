b62ee3b952042fe5e58dcaa841af7b8c2c6ad143
Tim Howarth <tim.howarth13@gmail.com>
2018-10-07 07:40:37 +0000

    Commit at shutdown

--- whatsnearme.js
+++ whatsnearme.js
@@ -6,7 +6,7 @@ const port=process.env.PORT || 3000 // declare port for heroku.
 var pagecode="<html><head></head><body>";
 var pageinfo="Last refresh: "+dt.myDateTime()+"</br>"+"<a href='https://github.com/timho13/WhatsNearMe'>Github</a>"+"</br>";
 var footer="</body></html>";
-var pagecode+=pageinfo;
+pagecode+=pageinfo;
 var body_code_list = {
     a: 'a1_open_form.html',
     b: 'b1_open_map_frameset.html',
