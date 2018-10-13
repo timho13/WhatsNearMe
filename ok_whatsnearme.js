586b1768775b0930cd947e7944c311dc18c9a1a0
Tim Howarth <tim.howarth13@gmail.com>
2018-10-13 18:09:40 +0000

    Commit at shutdown

--- ok_whatsnearme.js
+++ ok_whatsnearme.js
@@ -2,24 +2,20 @@ const http = require('http'); //declare module for server create.
 const fs = require('fs'); // declare module for file operations.
 const dt = require('./utilities');
 const port=process.env.PORT || 3000 // declare port for heroku.
-var pagecode="";
-var body_code_list = {
-  z: 'a0_top_of_html.html',
-  a: 'a1_open_form.html',
-  b: 'b1_open_map_frameset.html',
-  c: 'b2_close_frameset.html',
-  d: 'c1_open_item_playing_frameset.html',
-  e: 'b2_close_frameset.html',
-  f: 'd1_open_actions_frameset.html',
-  g: 'b2_close_frameset.html',
-  h: 'a2_close_form.html'
-};
-for (var filetoread in body_code_list){
-  fs.readFile(body_code_list[filetoread], function(err, data1) {
+var pagecode="<!DOCTYPE html><html><head>";
+var html_code_array = [
+  'a0_top_of_html.txt',
+  'a1_open_form.txt',
+  'b1_open_map_frameset.txt',
+  'c1_open_item_playing_frameset.txt',
+  'd1_open_actions_frameset.txt',
+  'a2_close_form.txt'
+];
+html_code_array.forEach(function(item, index, array){
+  fs.readFile(item, function(err, data1) {
     pagecode+=data1;
   });
-};
-//pagecode+="Last refresh: "+dt.myDateTime()+"</br>"+"<a href='https://github.com/timho13/WhatsNearMe'>Github</a>"+"</br>";
+});
 pagecode+="</body></html>";
 // server responds.
 var server = http.createServer(function (req, res) {
--- whatsnearme.js
+++ whatsnearme.js
@@ -2,9 +2,66 @@ const http = require('http'); //declare module for server create.
 const fs = require('fs'); // declare module for file operations.
 const dt = require('./utilities');
 const port=process.env.PORT || 3000 // declare port for heroku.
-var pagecode="<!DOCTYPE html><html><head>";
+var pagecode="<!DOCTYPE html><html><head><meta charset='UTF-8'/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<style>
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  /* Mobile Styles */
+  @media only screen and (max-width: 414px) {
+    body {
+      background-color: #000000;
+    }
+  }
+  /* Tablet & Desktop Styles */
+  @media only screen and (min-width: 415px) {
+    body {
+      background-color: #B2D6FF; /* Blue */
+        width: 414px;
+    }
+  }
+  body {
+    color: blue;
+  }
+  form {
+    border-radius: 25px;
+    border: 2px solid #73AD21;
+    padding: 20px;
+    background-color: #FFFFFF;
+  }
+  div {
+    display: block;
+  }
+  p {
+    font-size: 14px;
+  }
+
+  .button {
+    background-color: #4CAF50; /* Green */
+    border: none;
+    color: white;
+    padding: 20px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+  }
+
+  .button1 {border-radius: 2px;}
+  .button2 {border-radius: 4px;}
+  .button3 {border-radius: 8px;}
+  .button4 {border-radius: 12px;}
+  .button5 {border-radius: 50%;}
+</style>
+</head>
+<body>";
 var html_code_array = [
-  'a0_top_of_html.txt',
+//  'a0_top_of_html.txt',
   'a1_open_form.txt',
   'b1_open_map_frameset.txt',
   'c1_open_item_playing_frameset.txt',
