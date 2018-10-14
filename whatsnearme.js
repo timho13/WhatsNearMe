18e22d1cfe7b83b76713850f364520a870e6057a
Tim Howarth <tim.howarth13@gmail.com>
2018-10-14 07:34:32 +0000

    Commit at shutdown

--- whatsnearme.js
+++ whatsnearme.js
@@ -2,9 +2,11 @@ const http = require('http'); //declare module for server create.
 const fs = require('fs'); // declare module for file operations.
 const dt = require('./utilities');
 const port=process.env.PORT || 3000 // declare port for heroku.
-var pagecode=headstyle();
+
+var pagecode="<!DOCTYPE html><html><head>";
+//var pagecode=headstyle();
 var html_code_array = [
-//  'a0_top_of_html.txt',
+  'a0_top_of_html.txt',
   'a1_open_form.txt',
   'b1_open_map_frameset.txt',
   'c1_open_item_playing_frameset.txt',
